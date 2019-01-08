d3e8b127ab7bbd8914b542dad21d05f891beadb7
hendzee <hendzcode@gmail.com>
2019-01-08 13:57:01 +0700

    build dashboard

--- app/AssetsData.php
+++ app/AssetsData.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use App\ProjectPayment;
 use App\WorkerSalary;
 use App\ProjectBonus;
@@ -11,40 +12,59 @@ use App\Mutation;
 
 class AssetsData extends Model
 {
-    public function getCompanyAssets()
+    public function getCompanyAsset()
     {        
-        //get profit project
-        $project_payment = ProjectPayment::where('id_project', $id)
+        $company_asset = 0;
+
+        //project assset
+        $project_payment = DB::table('project')
+            ->join('project_payment', 'project.id_project', '=', 'project_payment.id_project' )
+            ->where('status', 'SELESAI')
             ->get()
             ->sum('transfer');
 
-        $worker_payment = WorkerSalary::select(DB::raw('sum(salary * fullday) as total'))
-            ->where('id_project', $id)
+        $worker_payment = DB::table('project')
+            ->join('worker_salary', 'project.id_project', '=', 'worker_salary.id_project')
+            ->where('project.status', 'SELESAI')
+            ->select(DB::raw('sum((salary * fullday) + (salary * halfday * 0.5)) as total'))
             ->first();
-        
-        $project_bonus = ProjectBonus::where('id_project', $id)
+
+        $project_bonus = DB::table('project')
+            ->join('project_bonus', 'project.id_project', '=', 'project_bonus.id_project')
+            ->where('project.status', 'SELESAI')
             ->get()
             ->sum('bonus');
-
-        $project_purchase = ProjectPurchase::select(DB::raw('sum(price_per_item * total_item) as total'))
-            ->where('id_project', $id)
-            ->first();
-
-        $mutation_in = Mutation::where('destiny', $id)
-            ->get()
-            ->sum('nominal');
-
-        $mutation_out = Mutation::where('source', $id)
-            ->get()
-            ->sum('nominal');
-
-        $income = $project_payment + $mutation_in;
         
-        $outcome = ($worker_payment->total) + $project_bonus + ($project_purchase->total)
-            + $mutation_out;
+        $project_mutation_in = DB::table('project')
+            ->join('mutation', 'project.id_project', '=', 'mutation.destiny')
+            ->where('destiny', '<>', 'PERUSAHAAN')
+            ->where('project.status' , 'SELESAI')
+            ->get()
+            ->sum('nominal');
 
-        $assets = $income - $outcome;
-        $profit = ($assets / $income) * 100;
-        //end get profit project        
+        $project_mutation_out = DB::table('project')
+            ->join('mutation', 'project.id_project', '=', 'mutation.source')
+            ->where('source', '<>', 'PERUSAHAAN')
+            ->where('project.status' , 'SELESAI')
+            ->get()
+            ->sum('nominal');
+
+        $project_purchase = DB::table('project')
+            ->join('project_purchase', 'project.id_project', '=', 'project_purchase.id_project')    
+            ->select(DB::raw('sum(price_per_item * total_item) as total'))
+            ->where('project.status', 'SELESAI')
+            ->first();        
+
+        $project_income = $project_payment + $project_mutation_in;
+        
+        $project_outcome = ($worker_payment->total) + $project_bonus + ($project_purchase->total)
+            + $project_mutation_out;
+
+        $project_asset = $project_income - $project_outcome;        
+        //end project asset
+
+        //project income from mutation
+
+        echo $project_asset;        
     }
 }
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\AssetsData;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $assets_data = new AssetsData();        
+
+        return $assets_data->getCompanyAsset();
         
-        return view('dashboard.index');
+        // return view('dashboard.index');
     }
 }
