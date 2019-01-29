6f8742cf3d152882185cb89a776b3c6cb2e9d985
hendzee <hendzcode@gmail.com>
2019-01-30 00:01:08 +0700

    repair and used button edit changed

--- resources/views/warehouse/repair_and_used/index.blade.php
+++ resources/views/warehouse/repair_and_used/index.blade.php
@@ -75,22 +75,22 @@
                                         }}
                                     </td>                                    
                                     <td>
-                                        <div class="btn-group">
-                                            <button class="btn btn-xs green dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-                                                AKSI <i class="fa fa-angle-down"></i>
-                                            </button>
-                                            <ul class="dropdown-menu" role="menu">
-                                                <li>
-                                                    <a href="{{ route('repair_and_used.edit', $data->item_name) }}">
-                                                        <i class="icon-docs"></i> Edit 
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href="">
-                                                        <i class="icon-tag"></i> Hapus
-                                                    </a>
-                                                </li>                                                
-                                            </ul>
+                                        <div class="row button-on-table">
+                                            <div class="col-xs-12">
+                                                <a href="{{ route('repair_and_used.edit', $data->item_name) }}" class="btn btn-icon-only green">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                            </div>
+                                            <div class="col-xs-6">
+                                                <form action="{{ action('@destroy', ) }}" method="POST">
+                                                    {{ method_field('DELETE') }}
+                                                    {{ csrf_field() }}
+
+                                                    <button type="submit" class="btn btn-icon-only red">
+                                                        <i class="fa fa-remove"></i>
+                                                    </button>
+                                                </form>
+                                            </div>
                                         </div>
                                     </td>
                                 </tr>
