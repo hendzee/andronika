d2b8cef2f6f81e6535025457e3d03cee11af0cc8
hendzee <hendzcode@gmail.com>
2019-02-10 13:27:30 +0700

    build dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\AssetsData;
+use App\Project;
 
 class DashboardController extends Controller
 {
@@ -18,7 +19,17 @@ class DashboardController extends Controller
         $data_profit = $assets_data->getCompanyProfit();
         $data_obli = $assets_data->getSalaryObligation();
         
+        $data_project_1 = Project::where('island', 'LUWUK')
+            ->orderBy('start', 'ASC')
+            ->get();
+        $data_project_2 = Project::where('island', 'BANGGAI LAUT')
+            ->orderBy('start', 'ASC')
+            ->get();
+        $data_project_3 = Project::where('island', 'BANGGAI KEPULAUAN')
+            ->orderBy('start', 'ASC')
+            ->get();
+        
         return view('dashboard.index', compact('data_income', 'data_outcome', 'data_profit', 
-            'data_obli'));
+            'data_obli', 'data_project_1', 'data_project_2', 'data_project_3'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -86,102 +86,123 @@
         <div class="clearfix"></div>
         <!-- END DASHBOARD STATS 1-->
         <div class="row">
-            <div class="col-lg-6 col-xs-12 col-sm-12">
-                <!-- BEGIN PORTLET-->
+            <div class="col-md-6">
+                <!-- Begin: life time stats -->
                 <div class="portlet light bordered">
                     <div class="portlet-title">
                         <div class="caption">
-                            <i class="icon-bar-chart font-dark hide"></i>
-                            <span class="caption-subject font-dark bold uppercase">Site Visits</span>
-                            <span class="caption-helper">weekly stats...</span>
-                        </div>
-                        <div class="actions">
-                            <div class="btn-group btn-group-devided" data-toggle="buttons">
-                                <label class="btn red btn-outline btn-circle btn-sm active">
-                                    <input type="radio" name="options" class="toggle" id="option1">New</label>
-                                <label class="btn red btn-outline btn-circle btn-sm">
-                                    <input type="radio" name="options" class="toggle" id="option2">Returning</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="portlet-body">
-                        <div id="site_statistics_loading">
-                            <img src="../assets/global/img/loading.gif" alt="loading" /> </div>
-                        <div id="site_statistics_content" class="display-none">
-                            <div id="site_statistics" class="chart"> </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- END PORTLET-->
-            </div>
-            <div class="col-lg-6 col-xs-12 col-sm-12">
-                <!-- BEGIN PORTLET-->
-                <div class="portlet light bordered">
-                    <div class="portlet-title">
-                        <div class="caption">
-                            <i class="icon-share font-red-sunglo hide"></i>
-                            <span class="caption-subject font-dark bold uppercase">Revenue</span>
-                            <span class="caption-helper">monthly stats...</span>
+                            <i class="icon-share font-blue"></i>
+                            <span class="caption-subject font-blue bold uppercase">Projek</span>
+                            <span class="caption-helper">Terbaru</span>
                         </div>
                         <div class="actions">
                             <div class="btn-group">
-                                <a href="" class="btn dark btn-outline btn-circle btn-sm dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"> Filter Range
-                                    <span class="fa fa-angle-down"> </span>
+                                <a class="btn green btn-sm" href="javascript:;" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"> 
+                                    LIHAT SEMUA
                                 </a>
-                                <ul class="dropdown-menu pull-right">
-                                    <li>
-                                        <a href="javascript:;"> Q1 2014
-                                            <span class="label label-sm label-default"> past </span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;"> Q2 2014
-                                            <span class="label label-sm label-default"> past </span>
-                                        </a>
-                                    </li>
-                                    <li class="active">
-                                        <a href="javascript:;"> Q3 2014
-                                            <span class="label label-sm label-success"> current </span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;"> Q4 2014
-                                            <span class="label label-sm label-warning"> upcoming </span>
-                                        </a>
-                                    </li>
-                                </ul>
                             </div>
                         </div>
                     </div>
                     <div class="portlet-body">
-                        <div id="site_activities_loading">
-                            <img src="../assets/global/img/loading.gif" alt="loading" /> </div>
-                        <div id="site_activities_content" class="display-none">
-                            <div id="site_activities" style="height: 228px;"> </div>
-                        </div>
-                        <div style="margin: 20px 0 10px 30px">
-                            <div class="row">
-                                <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
-                                    <span class="label label-sm label-success"> Revenue: </span>
-                                    <h3>$13,234</h3>
+                        <div class="tabbable-line">
+                            <ul class="nav nav-tabs">
+                                <li class="active">
+                                    <a href="#project_1" data-toggle="tab"> KOTA LUWUK </a>
+                                </li>
+                                <li>
+                                    <a href="#project_2" data-toggle="tab"> BANGGAI LAUT </a>
+                                </li>
+                                <li>
+                                    <a href="#overview_3" data-toggle="tab"> BANGGAI KEPULAUAN </a>
+                                </li>
+                            </ul>
+                            <div class="tab-content">
+                                <div class="tab-pane active" id="project_1">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped table-hover table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th> ID Projek </th>
+                                                    <th> Nama </th>
+                                                    <th> Tanggal </th>
+                                                    <th></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($data_project_1 as $data)
+                                                    <tr>
+                                                        <td>{{ $data->id_project }}</td>
+                                                        <td>{{ $data->name }}</td>
+                                                        <td>{{ $data->start }}</td>
+                                                        <td>
+                                                            <a href="javascript:;" class="btn btn-sm btn-default">
+                                                                <i class="fa fa-search"></i> Lihat </a>
+                                                        </td>
+                                                    </tr>    
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
-                                <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
-                                    <span class="label label-sm label-info"> Tax: </span>
-                                    <h3>$134,900</h3>
+                                <div class="tab-pane" id="project_2">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped table-hover table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th> ID Projek </th>
+                                                    <th> Nama </th>
+                                                    <th> Tanggal </th>
+                                                    <th></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($data_project_2 as $data)
+                                                    <tr>
+                                                        <td>{{ $data->id_project }}</td>
+                                                        <td>{{ $data->name }}</td>
+                                                        <td>{{ $data->start }}</td>
+                                                        <td>
+                                                            <a href="javascript:;" class="btn btn-sm btn-default">
+                                                                <i class="fa fa-search"></i> Lihat </a>
+                                                        </td>
+                                                    </tr>    
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
-                                <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
-                                    <span class="label label-sm label-danger"> Shipment: </span>
-                                    <h3>$1,134</h3>
-                                </div>
-                                <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
-                                    <span class="label label-sm label-warning"> Orders: </span>
-                                    <h3>235090</h3>
+                                <div class="tab-pane" id="project_3">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped table-hover table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th> ID Projek </th>
+                                                    <th> Nama </th>
+                                                    <th> Tanggal </th>
+                                                    <th></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($data_project_3 as $data)
+                                                    <tr>
+                                                        <td>{{ $data->id_project }}</td>
+                                                        <td>{{ $data->name }}</td>
+                                                        <td>{{ $data->start }}</td>
+                                                        <td>
+                                                            <a href="javascript:;" class="btn btn-sm btn-default">
+                                                                <i class="fa fa-search"></i> Lihat </a>
+                                                        </td>
+                                                    </tr>    
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- END PORTLET-->
+                <!-- End: life time stats -->
             </div>
         </div>
     </div>
